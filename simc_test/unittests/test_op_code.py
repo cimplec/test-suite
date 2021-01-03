b42f26608c9d219fe1fc0953c2a28598b7bd6af3
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-01-04 01:08:28 +0530

    Add non matching case in opcode tests

--- simc_test/unittests/test_op_code.py
+++ simc_test/unittests/test_op_code.py
@@ -7,7 +7,12 @@ class TestOpCode(unittest.TestCase):
     ####################################################################################################
     # TESTS
     ####################################################################################################
-    def test___str__(self):
+    def test___str__match_case(self):
         opcode = OpCode(opcode="print", val='"Hello World"')
 
         self.assertEqual(str(opcode), str(OpCode('print', '"Hello World"', None)))
+
+    def test___str__no_match_case(self):
+        opcode = OpCode(opcode="var_assign", val='a--1+2')
+
+        self.assertNotEqual(str(opcode), str(OpCode('print', '"Hello World"', None)))

e9d11ecdc29e88e8f4df557ee06ff99cdf4610fc
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-02-18 02:09:36 +0530

    Fix tests for array initializer

--- simc_test/unittests/parser/test_array_parser.py
+++ simc_test/unittests/parser/test_array_parser.py
@@ -34,6 +34,7 @@ class TestArrayParser(unittest.TestCase):
                 table=table,
                 size_of_array=2,
                 msg="Test message",
+                func_ret_type={}
             )
 
         self.__release_print()
@@ -134,7 +135,7 @@ class TestArrayParser(unittest.TestCase):
         table.entry("2", "int", "variable")
 
         op_value, op_type, i = array_initializer(
-            tokens=tokens_list, i=1, table=table, size_of_array=2, msg="Test message"
+            tokens=tokens_list, i=1, table=table, size_of_array=2, msg="Test message", func_ret_type={}
         )
 
         self.assertEqual(op_value, "{1,2")
